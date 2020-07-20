6a9196a06327bed5266fdbe04fac4dc2375afcc6
Adam Palmer <theadampalmer@gmail.com>
2020-07-20 15:01:41 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,8 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  word.match(/^[un]/)
-  word.match(/)
+  word.match(/^[un]//[ing$]/) 
 end
 
 def words_five_letters_long(text)
